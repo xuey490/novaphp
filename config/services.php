70c4d3ed4e6161616803fc0f2ea8c51c3bd478d0
xuey490 <xuey863toy@gmail.com>
2025-10-12 11:25:04 +0800

    增加upload类
    完成注册服务以及控制器测试

--- config/services.php
+++ config/services.php
@@ -325,7 +325,11 @@ return function (ContainerConfigurator $configurator) {
 	*/
 	
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 	// 第二种方法
+=======
+	// 第二种方法 注册thinkTemp
+>>>>>>> Stashed changes
 =======
 	// 第二种方法 注册thinkTemp
 >>>>>>> Stashed changes
@@ -351,7 +355,10 @@ return function (ContainerConfigurator $configurator) {
         ->public(); // 允许从容器外部获取
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 =======
+=======
+>>>>>>> Stashed changes
 	
     // 注册 MIME 检查器
     $services->set(\Framework\Utils\MimeTypeChecker::class)
@@ -362,6 +369,9 @@ return function (ContainerConfigurator $configurator) {
 
     $services->set(\Framework\Utils\FileUploader::class)
              ->args([$uploadConfig, service(\Framework\Utils\MimeTypeChecker::class)])->public();	
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 
 	
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -2,9 +2,15 @@
     'root' => array(
         'name' => 'blue2004/novaphp',
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
         'reference' => '22c6094f3d24a2ecada8ae749421f3dc3fd31774',
+=======
+        'pretty_version' => '0.0.10.x-dev',
+        'version' => '0.0.10.9999999-dev',
+        'reference' => 'b5e9ad8421cb3345a3b44e7f6e0cd3051ec8d112',
+>>>>>>> Stashed changes
 =======
         'pretty_version' => '0.0.10.x-dev',
         'version' => '0.0.10.9999999-dev',
@@ -18,9 +24,15 @@
     'versions' => array(
         'blue2004/novaphp' => array(
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
             'reference' => '22c6094f3d24a2ecada8ae749421f3dc3fd31774',
+=======
+            'pretty_version' => '0.0.10.x-dev',
+            'version' => '0.0.10.9999999-dev',
+            'reference' => 'b5e9ad8421cb3345a3b44e7f6e0cd3051ec8d112',
+>>>>>>> Stashed changes
 =======
             'pretty_version' => '0.0.10.x-dev',
             'version' => '0.0.10.9999999-dev',
