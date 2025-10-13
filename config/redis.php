684abbee63cff6d01ccbd97ec82d73a30ce0fd47
xuey490 <xuey863toy@gmail.com>
2025-10-13 21:13:23 +0800

    缓存类

--- config/redis.php
+++ config/redis.php
@@ -1,9 +1,11 @@
 <?php
 // config/redis.php
+//$_ENV['REDIS_HOST']
+
 return [
-    'host'     => $_ENV['REDIS_HOST'] ?? '127.0.0.1',
-    'port'     => (int) ($_ENV['REDIS_PORT'] ?? 6379),
-    'database' => (int) ($_ENV['REDIS_DB'] ?? 0),
-    'password' => $_ENV['REDIS_PASSWORD'] ?? null,
+    'host'     =>  env('REDIS_HOST') ?? '127.0.0.1',
+    'port'     => (int) (env('REDIS_PORT') ?? 6379),
+    'database' => (int) (env('REDIS_DB') ?? 0),
+    'password' => env('REDIS_PASSWORD') ?? null,
     'timeout'  => 2.5,
 ];
