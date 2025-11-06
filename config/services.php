be474fc34d438d0e420000d3be2ad8506493e533
xuey490 <xuey863toy@gmail.com>
2025-11-07 00:23:53 +0800

    Update services.php

--- config/services.php
+++ config/services.php
@@ -306,22 +306,19 @@ return function (ContainerConfigurator $configurator) {
 		->public();
 	
 	// 注册jwt服务
-<<<<<<< Updated upstream
-	$jwtConfig = require __DIR__ . '/../config/jwt.php';
-=======
->>>>>>> Stashed changes
+
 	$services->set('jwt' , \Framework\Utils\JwtFactory::class)
 		->autowire()
 		->public();	
 		
 	// 注册cookie服务
 	
-    $cookieConfig = require __DIR__ . '/../config/cookie.php';
+    $cookieConfig = __DIR__ . '/../config/cookie.php';
     // 注册 Cookie 服务，并传入配置
-    $services->set('cookie', \Framework\Utils\Cookie::class)
+    $services->set('cookie', \Framework\Utils\CookieManager::class)
 		->args([$cookieConfig])
         ->public();
-    $services->set(\Framework\Utils\Cookie::class)
+    $services->set(\Framework\Utils\CookieManager::class)
 		->args([$cookieConfig])
         ->public();
       
