502bc3539a896bfd4b06d680ceda3db2f5750c09
xuey490 <xuey863toy@gmail.com>
2025-11-15 19:37:59 +0800

    Update helpers.php

--- framework/helpers.php
+++ framework/helpers.php
@@ -23,10 +23,43 @@ use Framework\Cache\ThinkAdapter;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-
+use think\Validate;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
+
+
+
+
+if (!function_exists('validate')) {
+    /**
+     * 生成验证对象
+     * @param array $data 数据
+     * @param string|array $validate 验证器类名或者验证规则数组
+     * @param array $message 错误提示信息
+     * @param bool $batch 是否批量验证
+     * @param bool $failException 是否抛出异常
+     * @return  bool
+     */
+    function validate(array $data, $validate = '', array $message = [], bool $batch = false, bool $failException = true):bool
+    {
+        if (is_array($validate)) {
+            $v = new Validate();
+            $v->rule($validate);
+        } else {
+            if (strpos($validate, '.')) {
+                [$validate, $scene] = explode('.', $validate);
+            }
+            $v = new $validate();
+            if (!empty($scene)) {
+                $v->scene($scene);
+            }
+        }
+
+        return $v->message($message)->batch($batch)->failException($failException)->check($data);
+    }
+}
+
 
 /**
  * 开发辅助函数.
